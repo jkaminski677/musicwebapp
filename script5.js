aa28c2f74ee30e465ee9c99b71fe0a969945a4cc
Jan <jkaminski677@gmail.com>
2023-03-22 00:17:11 +0100

    added working arrows to change page to view

--- script5.js
+++ script5.js
@@ -5,8 +5,9 @@ var pdfViewer = document.querySelector('.canvasDiv');
 var thisScale = document.querySelector('.div-canvas-Scale');
 var rangeslider = document.getElementById("sliderRange");
 var output = document.getElementById("demo");
-var allPagesButton = document.getElementById('allpages');
-
+var stopVar = 0;
+var maxi = 0;
+document.getElementById('page_num').textContent = 1;
 output.innerHTML = rangeslider.value;
 
 const url = 'elements/element1/Wellerman_3st.pdf';
@@ -14,11 +15,28 @@ const url = 'elements/element1/Wellerman_3st.pdf';
 // Inicjalizacja PDF.js
 pdfjsLib.getDocument(url).promise.then(pdf => {
   // Pętla przez wszystkie strony pliku PDF
+  for (let selees = -2; selees <= 0; selees++){
+    const sele = document.createElement('div');
+    sele.id = `sel-${selees}`;
+    document.getElementById('Div-canvas-Scale').appendChild(sele);
+    const seleDiv = document.getElementById('sel-' + selees);
+    seleDiv.style.width = "100%";
+    seleDiv.style.height = "1px";
+    // seleDiv.style.border = "2px solid peru";
+  }
+
   for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
     // Utworzenie elementu canvas dla każdej strony
+    const sele = document.createElement('div');
+    sele.id = `sel-${pageNum}`;
+    document.getElementById('Div-canvas-Scale').appendChild(sele);
+    const seleDiv = document.getElementById('sel-' + pageNum);
+    seleDiv.style.width = "100%";
+    seleDiv.style.height = "1px";
+    // seleDiv.style.border = "2px solid peru";
+
     const canvas = document.createElement('canvas');
     canvas.id = `page-${pageNum}`;
-    
     document.getElementById('Div-canvas-Scale').appendChild(canvas);
     const currentDiv = document.getElementById('page-' + pageNum);
     currentDiv.style.width = "100%";
@@ -37,8 +55,58 @@ pdfjsLib.getDocument(url).promise.then(pdf => {
       };
       page.render(renderContext);
     });
+
+    pdfViewer.addEventListener('scroll', function() {
+      var element = document.querySelector('#page-' + pageNum);
+      var position = element.getBoundingClientRect();    
+
+      if(position.top < window.innerHeight && position.bottom >= 0) {
+        if (pageNum != stopVar) {
+            document.getElementById('page_num').textContent = pageNum;
+            changePage(pageNum, pdf.numPages);
+        }
+        stopVar = pageNum;
+      }
+    });
+
+    document.getElementById('page_count').textContent = pdf.numPages;
+  }
+
+  for (let selees = pdf.numPages+1; selees <= pdf.numPages+3; selees++){
+    const sele = document.createElement('div');
+    sele.id = `sel-${selees}`;
+    document.getElementById('Div-canvas-Scale').appendChild(sele);
+    const seleDiv = document.getElementById('sel-' + selees);
+    seleDiv.style.width = "100%";
+    seleDiv.style.height = "1px";
+    // seleDiv.style.border = "2px solid peru";
   }
 });
+
+
+
+function changePage(numero, maxi) {
+  function onNextPage() {
+    if (numero < maxi) {
+      numero++;
+      var leme = document.querySelector('#sel-' + numero);
+      leme.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+  document.getElementById('next').addEventListener('click', onNextPage);
+  
+  function onPrevPage() {
+    if(numero > 1){
+      numero--;
+      var leme = document.querySelector('#sel-' + numero);
+      leme.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+  document.getElementById('prev').addEventListener('click', onPrevPage);
+    
+}
+
+
 
 //////////////////// ZOOMING ////////////////////////////
 rangeslider.oninput = function() {
@@ -77,8 +145,6 @@ function zoom(event) {
       pdfViewer.style.alignItems = 'center';
     }
   }
-  
 }
-
 pdfViewer.onwheel = zoom;
 //////////////////// END ZOOMING ////////////////////////////
