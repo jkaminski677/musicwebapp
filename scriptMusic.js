3f4b7f6e1e9c133ea0d104002fd16434d5102f94
Jan <jkaminski677@gmail.com>
2023-03-23 19:30:47 +0100

    added some style

--- scriptMusic.js
+++ scriptMusic.js
@@ -2,46 +2,88 @@
 
 const audio = document.getElementById("audio");
 const playBtn = document.getElementById("play");
-const pauseBtn = document.getElementById("pause");
+const playButtonImg = document.getElementById("playButtonImg");
 const stopBtn = document.getElementById("stop");
-const loopCheckbox = document.getElementById("loop");
 const progress = document.getElementById("progress");
 const volumeSlider = document.getElementById("volume");
+const volumeImg = document.getElementById("volumeImg");
+
 // znaczniki
 const markStartBtn = document.getElementById("markStart");
 const markEndBtn = document.getElementById("markEnd");
 const loopBtn = document.getElementById("loopZn");
+const LoopTags = document.getElementById("LoopImg");
 const firstZn = document.querySelector(".progress-bar__marker--start");
 const secZn = document.querySelector(".progress-bar__marker--end");
 const resetZn = document.querySelector("#resetZn");
 let startZn = 0;
 let endZn = 0;
-let loopZn= false;
+let loopZn = false;
+// button on/off
+const toggleBtnImg = document.getElementById("toggleBtnImg");
+// tworzymy zmienną, która będzie przechowywać stan przycisku
+let isOn = false;
+toggleBtnImg.src = "img/no-repeat2.png";
+
+// dodajemy nasłuchiwanie na kliknięcie w przycisk
+toggleBtnImg.addEventListener("click", function () {
+  if (isOn) {
+    // jeśli przycisk był już włączony, to go wyłączamy
+    isOn = false;
+    audio.loop = isOn;
+    toggleBtnImg.src = "img/no-repeat2.png";
+  } else {
+    // jeśli przycisk był wyłączony, to go włączamy
+    isOn = true;
+    audio.loop = isOn;
+    toggleBtnImg.src = "img/repeatOne.png";
+  }
+});
+
+let PlayPause = true;
+playButtonImg.src = "img/play-button.png";
+// Play Button
+playBtn.addEventListener("click", function () {
+  if (PlayPause) {
+    // jeśli przycisk był wyłączony, to go włączamy
+    PlayPause = false;
+    playButtonImg.src = "img/pause.png";
+    playAudio();
+  } else {
+    // jeśli przycisk był już włączony, to go wyłączamy
+    PlayPause = true;
+    playButtonImg.src = "img/play-button.png";
+    pauseAudio();
+  }
+});
 
 
 markStartBtn.addEventListener("click", function () {
   startZn = audio.currentTime;
   var lle = startZn / audio.duration;
   firstZn.style.left = lle * 100 + "%";
-  firstZn.style.display = "block";
+  firstZn.style.display = "flex";
 });
 
 markEndBtn.addEventListener("click", function () {
   endZn = audio.currentTime;
   var lle = endZn / audio.duration;
   secZn.style.left = lle * 100 + "%";
-  secZn.style.display = "block";
+  secZn.style.display = "flex";
 });
 
-loopBtn.addEventListener("click", function () {
-  console.log(startZn);
-  console.log(endZn);
-  if (startZn > 0 && endZn > 0) {
-    
 
+// LoopTags.src = "img/play-button.png";
+LoopTags.addEventListener("click", function () {
+  if (startZn >= 0 && endZn >= 0) {
     loopZn = !loopZn;
     if (loopZn) {
-      audio.currentTime = startZn;
+        LoopTags.style.filter = "invert(45%) sepia(93%) saturate(416%) hue-rotate(179deg) brightness(85%) contrast(85%)";
+        LoopTags.style.transform = 'scale(1.5)';
+        audio.currentTime = startZn;
+    } else {
+        LoopTags.style.filter = "invert(86%) sepia(82%) saturate(5129%) hue-rotate(173deg) brightness(105%) contrast(105%)";
+        LoopTags.style.transform = 'scale(1)'
     }
   }
 });
@@ -55,7 +97,7 @@ resetZn.addEventListener("click", function () {
 });
 
 function playAudio() {
-    audio.play();
+  audio.play();
 }
 
 audio.addEventListener("timeupdate", function () {
@@ -76,10 +118,8 @@ function stopAudio() {
   startZn = 0;
   endZn = 0;
   loopZn = false;
-}
-
-function loopAudio() {
-  audio.loop = loopCheckbox.checked;
+  PlayPause = true;
+  playButtonImg.src = "img/play-button.png";
 }
 
 function updateProgress() {
@@ -93,15 +133,28 @@ function setProgress() {
 
 function setVolume() {
   audio.volume = volumeSlider.value;
+  if(audio.volume < 0.45)
+  {
+    volumeImg.src = "img/low-volume.png";
+  }
+  if (audio.volume < 0.1){
+    volumeImg.src = "img/zero-volume.png";
+    console.log("mniejsze");
+  } 
+  if (audio.volume > 0.45){
+    volumeImg.src = "img/medium-volume.png";
+  }
+  if (audio.volume > 0.85){
+    volumeImg.src = "img/max-volume.png";
+  }
 }
 
 // Event listeners
-playBtn.addEventListener("click", playAudio);
-pauseBtn.addEventListener("click", pauseAudio);
+// playBtn.addEventListener("click", playAudio);
+// pauseBtn.addEventListener("click", pauseAudio);
 stopBtn.addEventListener("click", stopAudio);
-loopCheckbox.addEventListener("change", loopAudio);
 audio.addEventListener("timeupdate", updateProgress);
-progress.addEventListener("change", setProgress);
+progress.addEventListener("input", setProgress);
 volumeSlider.addEventListener("input", setVolume);
 
 // time
@@ -124,8 +177,6 @@ function formatTime(time) {
   return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
-
-
 // open question
 const questionToggles = document.querySelectorAll(".question-toggle");
 
@@ -135,3 +186,4 @@ questionToggles.forEach((toggle) => {
     question.classList.toggle("open");
   });
 });
+
